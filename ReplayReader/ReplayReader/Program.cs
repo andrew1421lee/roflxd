575c0360e4fc89edc7c0774ed0497d23eda75e2b
Andrew Lee <andrew1421lee@gmail.com>
2018-12-26 11:04:30 +0800

    initial commit

---
+++ ReplayReader/ReplayReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplayReader
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: ReplayReader.exe [MODE] [PATH]");
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
