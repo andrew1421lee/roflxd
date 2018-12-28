182a47cf0e6650e823e486b38168446146d98157
Andrew Lee <andrew1421lee@gmail.com>
2018-12-28 20:09:07 +0800

    parses and prints basic information to conole

--- ReplayReader/ReplayReader/Program.cs
+++ ReplayReader/ReplayReader/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ReplayReader.Util;
+using System;
+using System.IO;
 
 namespace ReplayReader
 {
@@ -14,6 +12,20 @@ namespace ReplayReader
             {
                 Console.WriteLine("Usage: ReplayReader.exe [MODE] [PATH]");
                 Environment.Exit(-1);
+            }
+
+            string inputPath = args[0];
+
+            if(!File.Exists(inputPath))
+            {
+                Console.WriteLine($"Input file does not exist: {inputPath}");
+                Environment.Exit(-1);
+            }
+
+            using (var bReader = new BinaryReader(File.Open(inputPath, FileMode.Open)))
+            {
+                var result = HeaderReader.Read(bReader);
+                Console.WriteLine(result.ToString());
             }
         }
     }
