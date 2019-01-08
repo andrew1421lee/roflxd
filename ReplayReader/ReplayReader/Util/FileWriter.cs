f6dc2a18e248c0fc134384d2b59471ec7b3ce841
Andrew Lee <andrew1421lee@gmail.com>
2019-01-08 11:51:01 +0800

    implementing file writer

--- ReplayReader/ReplayReader/Util/FileWriter.cs
+++ ReplayReader/ReplayReader/Util/FileWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using ReplayReader.Models;
 
 namespace ReplayReader.Util
@@ -7,7 +8,20 @@ namespace ReplayReader.Util
     {
         public void WriteObject(string filePath, Header header)
         {
-            throw new System.NotImplementedException();
+            filePath += $"\\{header.DataHeader.MatchId}.json";
+            if(File.Exists(filePath)) { return; }
+
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine(header.Serialize());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[FileWriter] Encountered exception {ex.ToString()}");
+            }
         }
     }
 }
